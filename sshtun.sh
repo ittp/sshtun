178eb2ea178af147f3cd92ef2b8766c71b34305d
ittp <61617660+ittp@users.noreply.github.com>
2021-07-13 01:25:23 +0300

    Update sshtun.sh

--- sshtun.sh
+++ sshtun.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-
-# Strapi
-ssh -vvv is.ittps.ru -L 0.0.0.0:1337:localhost:1337 -N
-
 # SSH
-ssh -vvv is.ittps.ru -R 0.0.0.0:22:localhost:2222 -N
+KEY =""
+TUN_SSH=""
+ssh -vvv is.ittps.ru -R 0.0.0.0:22:localhost:2222  -L 0.0.0.0:5900:localhost:5900  -L 0.0.0.0:1337:localhost:1337 -N
 
-# Strapi
-ssh -vvv is.ittps.ru -L 0.0.0.0:5900:localhost:5900 -N
+
