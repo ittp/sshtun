ebbe17afac54948c66182ba699089805d21f71ed
ittp <61617660+ittp@users.noreply.github.com>
2021-07-13 01:32:37 +0300

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-ssh -vvv is.ittps.ru -R 0.0.0.0:22:localhost:2222  -L 0.0.0.0:5900:localhost:5900  -L 0.0.0.0:1337:localhost:1337 -N
-
-
+ssh -vvv is.ittps.ru -R 0.0.0.0:22:localhost:2222  -L 0.0.0.0:5900:localhost:5900  -L 0.0.0.0:1337:localhost:1337 -N &
