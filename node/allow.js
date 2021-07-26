99c225c78cc844696f4fef4279a253277759d643
ittp <61617660+ittp@users.noreply.github.com>
2021-07-26 19:49:21 +0300

    Create allow.js

---
+++ node/allow.js
@@ -0,0 +1,12 @@
+
+let ssh = require('ssh2')
+
+let ping = (host) => exec('ls')
+                  
+                  
+module.exports = [{id: 1, 
+              key: 'pdm', 
+              title: "data", 
+              ip: '94.26.139.111',
+              access: {}
+            }]
